4f69df492cee2f44c95b913a14af28df1f3a3860
NealeLegaspi <151595328+NealeLegaspi@users.noreply.github.com>
2024-01-10 09:46:03 +0800

    Add files via upload

---
+++ Cashier.java
@@ -0,0 +1,21 @@
+class Cashier {
+    private int number;
+    private boolean occupied;
+
+    Cashier(int number) {
+        this.number = number;
+        this.occupied = false;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    boolean isOccupied() {
+        return occupied;
+    }
+
+    void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+}
